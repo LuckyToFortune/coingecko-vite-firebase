cfce8e35947a99e456a0b19a1630e63fad7803ae
LuckyToFortune <167994680+LuckyToFortune@users.noreply.github.com>
2023-02-25 21:14:42 +0100

    my first commit. This is basically most of the UI and API implementations

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import AccountPage from "./routes/AccountPage";
+import CoinPage from "./routes/CoinPage";
+import axios from "axios";
+
+const App = () => {
+  const [coins, setCoins] = useState([]);
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data);
+      // console.log(response.data)
+    });
+  }, [url]);
+
+  return (
+    <ThemeProvider>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home coins = {coins}/>} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/acount" element={<AccountPage />} />
+
+        <Route path="/coin/:coinId" element={<CoinPage/>}>
+        <Route path=":coinId"/>
+        </Route>
+      </Routes>
+
+    
+
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import ThemeToggle from './ThemeToggle'
+import {AiOutlineClose, AiOutlineMenu} from "react-icons/ai"
+
+const NavBar = () => {
+
+    const [showNav, setShowNav] = useState(false)
+    const handleNav = () => {
+        setShowNav(!showNav)
+    }
+  return (
+    <div className='rounded-div flex items-center justify-between h-20 mx-auto font-semibold mt-2'>
+        <Link to = "/"> <h1 className='text-2xl'>Coin Pub</h1> </Link>
+
+        <div className='hidden md:flex'>
+            <ThemeToggle/>
+        </div>
+
+        <div className='hidden md:block'>
+            <Link className= "p-4 hover:text-accent" to = "/signIn">Sign In</Link>
+            <Link className= "bg-button text-buttonText px-5 py-2 ml-2 rounded-2xl shadow-lg hover:shadow-2xl" to = "/signUp">Sign Up</Link>
+        </div>
+
+        {/*mobile menu icon */}
+        <div className='flex md:hidden cursor-pointer z-10' onClick={handleNav}>
+            {
+                showNav ? <AiOutlineClose size={25}/> :  <AiOutlineMenu size={25}/>
+            }
+        </div>
+
+        {/*mobile menu*/}
+        <div className={
+            showNav ? 'flex flex-col md:hidden fixed left-0 top-20 items-center justify-between w-full h-[90%] bg-primary ease-in duration-300 z-10 '
+            :
+            'fixed left-[-100%] top-20 h-[90%] flex flex-col items-center justify-between ease-in'
+        }>
+            <ul className='w-full p-4'>
+                <li className='border-b py-6'> <Link to = "/">Home</Link> </li>
+                <li className='border-b py-6'> <Link to = "/account">Account</Link> </li>
+                <li className='border-b py-6'> <ThemeToggle/> </li>
+            </ul>
+            <div className='flex flex-col w-full p-4 '>
+                <Link to="/signIn"> <button className='w-full my-2 p-3 bg-primary border border-secondary rounded-2xl shadow-2xl'>Sign In</button> </Link>
+                <Link to="/signUp"> <button className='w-full my-2 p-3 bg-button text-buttonText border border-secondary rounded-2xl shadow-2xl'>Sign Up</button> </Link>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default NavBar

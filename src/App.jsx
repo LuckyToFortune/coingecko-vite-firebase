6da0d703bdbe77169945341d514beda2c26baf73
LuckyToFortune <167994680+LuckyToFortune@users.noreply.github.com>
2023-03-12 14:50:33 +0100

    modified the number of coins displayed from 10 to 100

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
   const [coins, setCoins] = useState([]);
 
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true";
 
   useEffect(() => {
     axios.get(url).then((response) => {
